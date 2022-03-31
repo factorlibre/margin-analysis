ea38c2c4a18b5580fc0798f8bfcfeac2cadd2c2a
OscarIndias <oscar.indias@factorlibre.com>
2022-03-31 16:57:32 +0200

    [UPDATE] product_standard_margin

---
+++ product_standard_margin/models/res_config_settings.py
@@ -0,0 +1,31 @@
+# © 2022 - FactorLibre - Oscar Indias <oscar.indias@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields, api
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    margin_tax = fields.Selection(
+        string="Tax in product margin",
+        selection=[("include", "Include"), ("exclude", "Exclude")],
+        readonly=True,
+    )
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        ir_config_sudo = self.env['ir.config_parameter'].sudo()
+        margin_tax = ir_config_sudo.get_param(
+            'product_standard_margin.margin_tax', False)
+        res.update(margin_tax=margin_tax)
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        ir_config_sudo = self.env['ir.config_parameter'].sudo()
+        ir_config_sudo.set_param(
+            'product_standard_margin.margin_tax',
+            self.margin_tax
+        )
